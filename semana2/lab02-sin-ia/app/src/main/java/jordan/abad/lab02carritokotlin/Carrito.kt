3dd5f7cd751ff0ad675b913d48f432f8f6c8f648
Jordan Abad <jordanabad797@gmail.com>
2026-08-26 02:16:22 -0500

    Commit 1: Proyecto inicial con menu del carrito

---
+++ semana2/lab02-sin-ia/app/src/main/java/jordan/abad/lab02carritokotlin/Carrito.kt
@@ -0,0 +1,7 @@
+package jordan.abad.lab02carritokotlin
+
+fun main() {
+    println("=========================================")
+    println("   CARRITO DE COMPRAS - TIENDA TECSUP   ")
+    println("=========================================")
+}
