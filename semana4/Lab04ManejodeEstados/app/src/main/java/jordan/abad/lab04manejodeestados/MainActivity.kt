20267462880f24b3924af76d33c504de9ac4cc79
Jordan Abad <jordanabad797@gmail.com>
2026-09-16 01:31:36 -0500

    4. feat: implementar TaskApp con manejo de estado de la lista de tareas

--- semana4/Lab04ManejodeEstados/app/src/main/java/jordan/abad/lab04manejodeestados/MainActivity.kt
+++ semana4/Lab04ManejodeEstados/app/src/main/java/jordan/abad/lab04manejodeestados/MainActivity.kt
@@ -6,9 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import jordan.abad.lab04manejodeestados.ui.theme.Lab04ManejoDeEstadosTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,17 +16,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             Lab04ManejoDeEstadosTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    TemperatureDisplay()
+                    TaskApp()
                 }
             }
         }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun TemperatureDisplayPreview() {
-    Lab04ManejoDeEstadosTheme {
-        TemperatureDisplay()
     }
 }
